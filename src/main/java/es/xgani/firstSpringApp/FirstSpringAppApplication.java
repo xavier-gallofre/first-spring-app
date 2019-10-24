0f1380b541156f10be3187d05a6fb9e5f119a1c4
Xavier Gallofré Nieva <xavier.gallofre@gmail.com>
2019-10-24 20:49:02 +0200

    Initial commit - Project created with Spring initializr from https://start.spring.io/

---
+++ src/main/java/es/xgani/firstSpringApp/FirstSpringAppApplication.java
@@ -0,0 +1,13 @@
+package es.xgani.firstSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/xgani/firstSpringApp/FirstSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package es.xgani.firstSpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
